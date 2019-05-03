1f34cfb137eef3d1c90af18df4dc878b42e1edf7
Yulius Figo Didin Kurnianto <yulius.2293@students.amikom.ac.id>
2019-05-03 14:39:26 +0700

    inisialisasi project

---
+++ Karyawan.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace responsipemrog2293
+{
+    public class Karyawan
+    {
+        public Karyawan()
+        {
+
+        }
+
+        public Karyawan(string nik, string nama, int gajibulanan)
+        {
+
+            if (GajiBulanan < 0)
+            {
+                Console.WriteLine("gaji harus daiatas 0");
+            }
+            else
+            {
+                Nik = nik;
+                Nama = nama;
+                GajiBulanan = gajibulanan;
+            }
+        }
+
+        public string Nik { get; set; }
+        public string Nama { get; set; }
+        public int GajiBulanan { get; set; }
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace responsipemrog2293
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Karyawan krysatu = new Karyawan();
+
+            krysatu.Nik = "33445566";
+            krysatu.Nama = "Waluyo";
+            krysatu.GajiBulanan = 2000000;
+
+            Karyawan krydua = new Karyawan();
+
+            krydua.Nik = "66554433";
+            krydua.Nama = "Tumijo";
+            krydua.GajiBulanan = 3000000;
+
+            Console.WriteLine("No. \tNIK/nama\t\tGaji Bulanan");
+            Console.WriteLine("------------------------------");
+            Console.WriteLine("1. \t{0} {1}\t\t{2}", krysatu.Nik, krysatu.Nama, krysatu.GajiBulanan);
+            Console.WriteLine("1. \t{0} {1}\t\t{2}", krydua.Nik, krydua.Nama, krydua.GajiBulanan);
+            Console.WriteLine("\n\n");
+            Console.WriteLine("Asiqqq kenaikan gaji 10% nih!!");
+            Console.WriteLine("\n");
+            Console.WriteLine("No. \tNIK/nama\t\tGaji Bulanan");
+            Console.WriteLine("------------------------------");
+            Console.WriteLine("1. \t{0} {1}\t\t{2}", krysatu.Nik, krysatu.Nama, krysatu.GajiBulanan + (krysatu.GajiBulanan * 0.10));
+            Console.WriteLine("1. \t{0} {1}\t\t{2}", krydua.Nik, krydua.Nama, krydua.GajiBulanan + (krysatu.GajiBulanan * 0.10));
+            Console.ReadKey();
+        }
+
+    }
+}
